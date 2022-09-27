39484b730d0351159a33e5a224792ceefbdfcff1
raymondmasime <vagrant@ubuntu-focal>
2022-09-27 05:34:38 +0000

    strstr task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,30 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * *_strstr - description
- * @haystack: string
- * @needle: pointer
- * Return: pointer
+ * _strstr - function that search array needle in array haystack
+ * @haystack: array where the functions will search
+ * @needle: array to search
+ * Return: pointer to the first character of the first occurence
+ * of the needle is returned. NULL: if needle not found.
+ * Haystack if needle is an empty string
  */
-
-
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;
+	int i = 0, j = 0;
+	char *p;
 
-for (i = 0; haystack[i] > '\0'; i++)
-{
-	for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+	if (needle[i] == '\0')
+		p = haystack;
+	while (haystack[i] && needle[j])
 	{
-		if (haystack[j] != needle[j - i])
-		{
-			break;
-		}
+		j = 0;
+		while (haystack[i + j] == needle[j] && needle[j])
+			j++;
+		if (needle[j] == '\0')
+			p = haystack + i;
+		else
+			p = 0;
+		i++;
 	}
-	if (needle[j - i] == '\0')
-	{
-		return (haystack + i);
-	}
-}
-return (0);
+	return (p);
 }
