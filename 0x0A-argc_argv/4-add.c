00e201f379c610e91dcbf98e157b3eef9f9d3609
raymondmasime <vagrant@ubuntu-focal>
2022-10-02 10:40:36 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main - prints sum of two numbers
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Description: prints sum of two numbers
+ * Return: Always(0) Success
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+int i, j, sum;
+sum = 0;
 
-	if (argc < 1)
-		return (0);
-
-	for (i = 1; i < argc; i++)
-	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
-		sum += atoi(argv[i]);
-	}
-	printf("%d\n", sum);
-
-	return (0);
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+return (puts("Error"), 1);
+}
+}
+sum += atoi(argv[i]);
+}
+return (printf("%d\n", sum), 0);
 }
