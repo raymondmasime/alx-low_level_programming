b1320b86bd03e849297f65f5f0834fcc2f88f29c
raymondmasime <vagrant@ubuntu-focal>
2022-10-24 16:59:13 +0000

    task fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,19 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
-  * free_list - frees a linked list
-  *
-  * @head: the head of the list to free
-  * Return: void
-  */
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list.
+ */
 void free_list(list_t *head)
 {
-	if (!head)
-		return;
-	if (head->next)
-		free_list(head->next);
-	free(head->str);
-	free(head);
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
 }
